3bd761b4a649a9b245d34d323396f75341a8db38
Sulagna Palit <sulagnapalit04@gmail.com>
2024-11-24 00:23:07 +0530

    some cha

--- allsort.cpp
+++ allsort.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 using namespace std;
+
+// Function to swap two elements in an array
 void swap(int arr[],int a,int b){
 	int temp=arr[a];
 	arr[a]=arr[b];
 	arr[b]=temp;
 }
-void SelectionSort(int arr[],int n){
+
+// Selection Sort implementation
+void selection_sort(int arr[],int n){
 	int i,j;
 	for(i=0;i<n-1;i++){
 		int smallestno_idx=i;
@@ -17,6 +21,8 @@ void SelectionSort(int arr[],int n){
 		swap(arr,i,smallestno_idx);
 	}
 }
+
+// Insertion Sort implementation
 void insertion_sort(int arr[],int n){
 	int i,j;
 	for(i=1;i<n;i++){
@@ -35,7 +41,9 @@ void insertion_sort(int arr[],int n){
 		arr[j+1]=key;
 	}
 }
-void Bubblesort(int arr[],int n){
+
+// Bubble Sort implementation
+void bubble_sort(int arr[],int n){
 	int i,j;
 	for (i=0;i<n-1;i++){
 		int isSort=0;
@@ -45,12 +53,15 @@ void Bubblesort(int arr[],int n){
 				isSort=1;
 			}
 		}
+        // If no two elements were swapped in inner loop, array is sorted
 		if(isSort==0){
 			return;
 		}
 		
 	}		
 }
+
+// Function to print the array
 void PrintArray(int arr[],int n){
 	int i;
 	for(i=0;i<n;i++){
@@ -58,3 +69,38 @@ void PrintArray(int arr[],int n){
 	}
 }
 
+int main() 
+{
+    int n, choice;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Choose a sorting algorithm:\n";
+    cout << "1. Selection Sort\n2. Insertion Sort\n3. Bubble Sort\n";
+    cin >> choice;
+
+    switch (choice) {
+        case 1:
+            selection_sort(arr, n);
+            break;
+        case 2:
+            insertion_sort(arr, n);
+            break;
+        case 3:
+            bubble_sort(arr, n);
+            break;
+        default:
+            cout << "Invalid choice.\n";
+    }
+
+    cout << "Sorted array: ";
+    PrintArray(arr, n);
+
+    return 0;
+}
